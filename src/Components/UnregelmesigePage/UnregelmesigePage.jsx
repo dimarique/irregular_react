713c841b60740f0d94bde5b78933ae214e9cf11f
dimarique <pochtagreen@gmail.com>
2025-02-06 17:46:39 +0100

    remove unnecessary import

--- src/Components/UnregelmesigePage/UnregelmesigePage.jsx
+++ src/Components/UnregelmesigePage/UnregelmesigePage.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Nav from "./../Nav/Nav"
 import Button from "./../Button/Button";
 import Card from "./../Card/Card";
-import VerbFormRow from "./../VerbFormRow/VerbFormRow";
 
 
 function Page(props) {
 	return <>
 		<Nav />
 		<Card />
-		<VerbFormRow />
 		<div className="buttons">
 			<Button text='testen' />
 			<Button text='nächste' />
